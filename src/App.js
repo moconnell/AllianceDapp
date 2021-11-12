1b8274975c5a5a064a6a4a7f41bd75e6002b2706
tikisailor <40813246+tikisailor@users.noreply.github.com>
2021-11-12 22:56:11 +0800

    layout profile & availablility w/ chakra ui

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import * as React from 'react'
 import './App.css'
 import {Calendar} from './components/Calendar'
 import { MeetingCard } from './components/MeetingCard'
+import { Input } from "./components/Input";
 export default function App() {
   return (
-    <main style={{background:"#333333",width:"vw",height:"vh",padding:"200px",display:"flex",justifyContent:"space-between"}}>
-          <Calendar onChange={(date)=>{console.log(date)}}/>
-          <MeetingCard attendee={"John Salchichon"} date={new Date()} endDate={new Date(new Date().setHours(23))} description={"Meeting to discuss the next steps in the Nazi World Domination Schedule "}/>
+    <main style={{background:"#333333",width:"vw",height:"vh",display:"flex",justifyContent:"space-between"}}>
+        {/*<Calendar onChange={(date)=>{console.log(date)}}/>*/}
+        {/*<MeetingCard attendee={"John Salchichon"} date={new Date()} endDate={new Date(new Date().setHours(23))} description={"Meeting to discuss the next steps in the Nazi World Domination Schedule "}/>*/}
+        <Input />
     </main>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import theme from "./theme";
+import "./theme/styles.css";
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider theme={ theme }>
+          <App />
+      </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
