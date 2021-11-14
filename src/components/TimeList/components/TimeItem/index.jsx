cfd5ab589d8bc60882701e9e131b1fdee6313453
Carlos Di Matteo <carlosdima_97@hotmail.com>
2021-11-14 01:17:52 -0500

    time button component

---
+++ src/components/TimeList/components/TimeItem/index.jsx
@@ -0,0 +1,10 @@
+import { getMeridian } from "../../../../utils/formatDate";
+import { TimeButton } from "./styles";
+
+export function TimeItem({hours,minutes,seconds,active,onClick}){
+    return (
+        <TimeButton  onClick={onClick} active={active}>
+            {getMeridian(hours,minutes)}
+        </TimeButton>
+    )
+}
