0ea3eadd1a70d3db2adf4708f3611164aa17714e
Carlos Di Matteo <carlosdima_97@hotmail.com>
2021-11-14 01:52:02 -0500

    fixed 12pm format date

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,4 +1,4 @@
 export function getMeridian(hour,minutes){
        const formattedMinutes =  minutes>9 ? minutes : `0${minutes}`
-        return hour > 12 ? `${hour-12}:${formattedMinutes}PM` : `${hour}:${formattedMinutes}AM`
+        return hour > 11 ? `${hour-12 || 12}:${formattedMinutes}PM` : `${hour}:${formattedMinutes}AM`
 }
