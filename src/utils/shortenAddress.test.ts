abf0cf0f3f348565b9395a3e09f282e8beb4158a
Matthew O'Connell <totallyterrific@tutanota.com>
2022-04-29 13:42:48 +0200

    update test

--- src/utils/shortenAddress.test.ts
+++ src/utils/shortenAddress.test.ts
@@ -2,6 +2,7 @@ import { shortenAddress } from "./shortenAddress";
 
 describe("shortenAddress", () => {
   const testData = [
+    { input: undefined, expected: undefined },
     { input: "me@mydomain.com", expected: "me@my...com" },
     { input: "mr_bluesky@tutanota.com", expected: "mr_bl...com" },
   ];
